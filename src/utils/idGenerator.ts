b4f33992f53127d83cf5480749b08e01ff3756e8
elek <elek>
2022-12-23 16:30:14 +0000

    better ids

--- src/utils/idGenerator.ts
+++ src/utils/idGenerator.ts
@@ -1,11 +1,30 @@
-import { randomBytes } from "crypto";
 import prisma from "./prismaClient";
 
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+const length = 4;
 export const id = async () => {
-    let id = randomBytes(6).toString("hex").slice(0, 4);
-    while (await prisma.paste.findUnique({ where: { id } })) {
-        id = randomBytes(3).toString("hex").slice(0, 4);
-        console.log("id already exists, generating new id", id);
+  let fail = 0
+  console.time('id')
+  let id = "";
+  for (let i = 0; i < length; i++) {
+    id = id + alphabet[Math.floor(Math.random() * alphabet.length)];
+  }
+  while (await prisma.paste.findUnique({ where: { id } })) {
+    fail++;
+    id = "";
+
+    for (let i = 0; i < length; i++) {
+      id = id + alphabet[Math.floor(Math.random() * alphabet.length)];
     }
-    return id;
+  }
+
+  fail > 0 ? console.error(fail) : null;
+  console.timeEnd('id')
+  return id;
 }
+
+/*
+Use a faster random number generator.
+Pre-generate and store a large number of random strings in an array.
+Keep track of a pool of unused strings and only generate new ones when necessary.
+*/
